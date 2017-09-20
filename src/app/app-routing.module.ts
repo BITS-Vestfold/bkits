7c2d62709df0195ed7d485efd9add9c163bdf6ae
SpacyCoder <sral_h@hotmail.com>
2017-09-20 16:28:21 +0200

    added about us component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
-
 import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
 import {MainComponent} from "./main/main.component";
 import {EventsComponent} from "./events/events.component";
 import {RegisterComponent} from "./register/register.component";
 import {ContactComponent} from "./contact/contact.component";
+import {AboutUsComponent} from "./about-us/about-us.component";
 
 const appRoutes: Routes = [
   {path: 'events', component: EventsComponent },
   {path: 'bli-medlem', component: RegisterComponent },
   {path: 'kontakt', component: ContactComponent },
+  {path: 'om-oss', component: AboutUsComponent },
   {path: '', redirectTo: '/main', pathMatch: 'full'},
   {path: 'main', component: MainComponent},
 ];
