14c8631cd990cb9eceb1901a1a22eaeca258d253
SpacyCoder <sral_h@hotmail.com>
2017-09-20 12:19:43 +0200

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FacebookService, InitParams} from 'ngx-facebook';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(private fb: FacebookService) {
+  }
+
+
+  ngOnInit(): void {
+    const initParams: InitParams = {
+      appId: '709255365937283',
+      xfbml: true,
+      version: 'v2.8'
+    };
+
+    this.fb.init(initParams);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MdToolbarModule} from '@angular/material';
+import { FacebookModule, FacebookService} from 'ngx-facebook';
+import { FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MdToolbarModule,
+    FlexLayoutModule,
+    FacebookModule.forRoot(),
+    BrowserAnimationsModule,
+    FlexLayoutModule
   ],
-  providers: [],
+  providers: [FacebookService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
