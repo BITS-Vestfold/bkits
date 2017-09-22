6741ba103f0eed398471ffbca418b3c17dde1df6
peveneven <eveha94@gmail.com>
2017-09-22 15:14:57 +0200

    Did some refactoring in main-component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -9,12 +9,7 @@ import * as RX from 'rxjs';
 })
 export class MainComponent implements OnInit {
 
-  sponsorImgs : any[] = [
-    {name: 'Syse', imgSrc: 'https://www.syse.no/assets/SYSE_logo_darkText_300x113.png'},
-    {name: 'Microsoft Norge', imgSrc: 'https://www.csra.com/sites/default/files/microsoft_logo.png'},
-    {name: 'Tesla Motors', imgSrc: 'https://www.blog.google/images/download/DvujqGIr46dew-phGARpDy8bMSQ=/1095/original/Google_Logo.png'},
-    {name: 'Valve', imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Valve_logo.svg/2000px-Valve_logo.svg.png'}
-  ];
+
   @ViewChild("mainContent") mainContent: ElementRef;
   THREE: any;
   mouseX = 100;
