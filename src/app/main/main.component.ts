67cb3199c9b06134b6d8823c60a9495a0dae51eb
Odin <odinhorst@hotmail.com>
2017-09-22 14:28:51 +0200

    cool 3d homescreen

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,Renderer2,ElementRef,ViewChild, OnInit ,AfterContentInit} from '@angular/core';
+import * as THREElib from 'three-js';
 
 @Component({
   selector: 'app-main',
@@ -13,10 +14,83 @@ export class MainComponent implements OnInit {
     {name: 'Tesla Motors', imgSrc: 'https://www.blog.google/images/download/DvujqGIr46dew-phGARpDy8bMSQ=/1095/original/Google_Logo.png'},
     {name: 'Valve', imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Valve_logo.svg/2000px-Valve_logo.svg.png'}
   ];
+  @ViewChild("mainContent") mainContent: ElementRef;
+  THREE: any;
+  mouseX = 100;
+  mouseY = 100;
+  windowHalfX = window.screenX/2;
+  windowHalfY =  window.screenY/2;
+  SEPARATION = 200;
+  AMOUNTX = 10;
+  AMOUNTY = 10;
+  camera: any;
+  scene: any;
+  renderer: any;
+  constructor(private rend: Renderer2) {
 
-  constructor() { }
+  }
 
   ngOnInit() {
+      console.log(window.innerWidth + " " + window.innerHeight)
+      this.THREE = THREElib(["Projector","CanvasRenderer"]);
+      this.rend.listen('body','mousemove',e => {
+          this.mouseX = e.clientX - window.screenX/2;
+          this.mouseY = e.clientY - window.screenY/2;
+          this.render();
+      });
+      this.windowHalfX = window.screenX/2;
+      this.windowHalfY = window.screenX/2;
+      let separation = 100, amountX = 50, amountY = 50,particles, particle;
+      this.camera = new this.THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+      this.camera.position.z = 100;
+      this.scene = new this.THREE.Scene();
+      this.renderer = new this.THREE.CanvasRenderer();
+      this.renderer.setPixelRatio( window.devicePixelRatio );
+      this.renderer.setSize(window.innerWidth,window.innerHeight-80);
+      this.renderer.setClearColorHex(0xffffff,1);
+      this.renderer.domElement.style.width="100%";
+      this.renderer.domElement.style.height="100%";
+      this.renderer.domElement.style.position="absolute";
+      this.mainContent.nativeElement.appendChild( this.renderer.domElement );
+      // particles
+      let PI2 = Math.PI * 2;
+      let material = new this.THREE.SpriteCanvasMaterial( {
+          color: 0xffffff,
+          program: function ( context ) {
+              context.beginPath();
+              context.arc( 0, 0, 0.5, 0, PI2, true );
+              context.fill();
+          }
+      } );
+      let geometry = new this.THREE.Geometry();
+      for ( var i = 0; i < 100; i ++ ) {
+          particle = new this.THREE.Sprite( material );
+          particle.position.x = Math.random() * 2 - 1;
+          particle.position.y = Math.random() * 2 - 1;
+          particle.position.z = Math.random() * 2 - 1;
+          particle.position.normalize();
+          particle.position.multiplyScalar( Math.random() * 10 + 450 );
+          particle.scale.x = particle.scale.y = 10;
+          //this.scene.add( particle );
+          geometry.vertices.push( particle.position );
+      }
+      // lines
+      let line = new this.THREE.Line( geometry, new this.THREE.LineBasicMaterial( { color: 0x000000, opacity: 0.5 } ) );
+      this.scene.add( line );
+  }
+  ngAfterContentInit()
+  {
+
+  }
+  animate() {
+      requestAnimationFrame( this.animate );
+      this.render();
+  }
+  render() {
+      this.camera.position.x += ( this.mouseX - this.camera.position.x ) * .05;
+      this.camera.position.y += ( - this.mouseY + 200 - this.camera.position.y ) * .05;
+      this.camera.lookAt( this.scene.position );
+      this.renderer.render( this.scene, this.camera );
   }
 
 }
