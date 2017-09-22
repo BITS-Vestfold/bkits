bbd8fcae45b5194072ce641956eaf973fc6401da
Odin <odinhorst@hotmail.com>
2017-09-22 18:27:34 +0200

    Swag center animation

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,88 +3,133 @@ import * as THREElib from 'three-js';
 import * as RX from 'rxjs';
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+    selector: 'app-main',
+    templateUrl: './main.component.html',
+    styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
 
 
-  @ViewChild("mainContent") mainContent: ElementRef;
-  THREE: any;
-  mouseX = 100;
-  mouseY = 100;
-  windowHalfX = window.screenX/2;
-  windowHalfY =  window.screenY/2;
-  SEPARATION = 200;
-  AMOUNTX = 10;
-  AMOUNTY = 10;
-  camera: any;
-  scene: any;
-  renderer: any;
-  constructor(private rend: Renderer2,private ngZone: NgZone) {
+    @ViewChild("mainContent") mainContent: ElementRef;
+    THREE: any;
+    mouseX = 100;
+    mouseY = 100;
+    windowHalfX = window.screenX/2;
+    windowHalfY =  window.screenY/2;
+    SEPARATION = 800;
+    AMOUNTX = 100;
+    AMOUNTY = 100;
+    camera: any;
+    scene: any;
+    renderer: any;
+    constructor(private rend: Renderer2,private ngZone: NgZone) {
 
-  }
+    }
 
-  ngOnInit() {
-      console.log(window.innerWidth + " " + window.innerHeight)
-      this.THREE = THREElib(["Projector","CanvasRenderer"]);
-      this.rend.listen('body','mousemove',e => {
-          this.mouseX = e.clientX - window.screenX/2;
-          this.mouseY = e.clientY - window.screenY/2;
-      });
-      this.windowHalfX = window.screenX/2;
-      this.windowHalfY = window.screenX/2;
-      let separation = 100, amountX = 50, amountY = 50,particles;
-      this.camera = new this.THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-      this.camera.position.z = 100;
-      this.scene = new this.THREE.Scene();
-      this.renderer = new this.THREE.CanvasRenderer();
-      this.renderer.setPixelRatio( window.devicePixelRatio );
-      this.renderer.setSize(window.innerWidth,window.innerHeight-80);
-      this.renderer.setClearColorHex(0xffffff,1);
-      this.renderer.domElement.style.width="100%";
-      this.renderer.domElement.style.height="100%";
-      this.renderer.domElement.style.position="absolute";
-      this.mainContent.nativeElement.appendChild( this.renderer.domElement );
-      // particles
-      let PI2 = Math.PI * 2;
-      let material = new this.THREE.SpriteCanvasMaterial( {
-          color: 0x017FD3,
-          program: function ( context ) {
-              context.beginPath();
-              context.arc( 0, 0, 0.5, 0, PI2, true );
-              context.fill();
-          }
-      } );
-      let geometry = new this.THREE.Geometry();
-      for ( var i = 0; i < 100; i ++ ) {
-          var particle = new this.THREE.Sprite( material );
-          particle.position.x = Math.random() * 2 - 1;
-          particle.position.y = Math.random() * 2 - 1;
-          particle.position.z = Math.random() * 2 - 1;
-          particle.position.normalize();
-          particle.position.multiplyScalar( Math.random() * 10 + 450 );
-          particle.scale.x = 10;
-          particle.scale.y = 10;
-          particle.geometry = new this.THREE.CircleGeometry();
-          this.scene.add( particle );
-          geometry.vertices.push( particle.position );
-      }
-      // lines
-      let line = new this.THREE.Line( geometry, new this.THREE.LineBasicMaterial( { color: 0x000000, opacity: 0.3 } ) );
-      this.scene.add( line );
+    ngOnInit() {
+        console.log(window.innerWidth + " " + window.innerHeight)
+        this.THREE = THREElib(["Projector","CanvasRenderer"]);
+        this.rend.listen('body','mousemove',e => {
+        let wd = window.screen.width;
+        if(e.clientX > wd/2)
+            {
+            this.mouseX = wd -  e.clientX;
+                }
+                else
+                {
+                    this.mouseX = e.clientX;
+                    }
+                    console.log(window.screen.width);
+            this.mouseY = e.clientY - window.screenY;
+        });
+        this.windowHalfX = window.screenX/2;
+        this.windowHalfY = window.screenX/2;
+        let separation = 1000, amountX = 100, amountY = 100,particles;
+        this.camera = new this.THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+        this.camera.position.z = 100;
+        this.scene = new this.THREE.Scene();
+        this.renderer = new this.THREE.CanvasRenderer();
+        this.renderer.setPixelRatio( window.devicePixelRatio );
+        this.renderer.setSize(window.innerWidth,window.innerHeight-80);
+        this.renderer.setClearColorHex(0xffffff,1);
+        this.renderer.domElement.style.width="100%";
+        this.renderer.domElement.style.height="100%";
+        this.renderer.domElement.style.position="absolute";
+        this.mainContent.nativeElement.appendChild( this.renderer.domElement );
+        // particles
+        let PI2 = Math.PI * 2;
+        let material = new this.THREE.SpriteCanvasMaterial( {
+            color: 0x017FD3,
+            program: function ( context ) {
+                context.beginPath();
+                context.arc( 0, 0, 0.5, 0, PI2, true );
+                context.fill();
+            }
+        } );
+        let geometry = new this.THREE.Geometry();
+        for ( var i = 0; i < 150; i ++ ) {
+            var particle = new this.THREE.Sprite( material );
+            particle.position.x = Math.random() * 2 - 1;
+            particle.position.y = Math.random() * 2 - 1;
+            particle.position.z = Math.random() * 2 - 1;
+            particle.position.normalize();
+            particle.position.multiplyScalar( Math.random() * 100 + 550 );
+            particle.scale.x = 10;
+            particle.scale.y = 10;
+            particle.geometry = new this.THREE.CircleGeometry();
+            this.scene.add( particle );
+            geometry.vertices.push( particle.position );
+        }
+        // lines
+        let line = new this.THREE.Line( geometry, new this.THREE.LineBasicMaterial( { color: 0x000000, opacity: 0.3 } ) );
+        this.scene.add( line );
+        //   var material = new this.THREE.SpriteCanvasMaterial( {
+        //       color: 0x000000,
+        //       program: function ( context ) {
+        //           context.beginPath();
+        //           context.arc( 0, 0, 0.5, 0, PI2, true );
+        //           context.fill();
+        //       }
+        //   } );
+        //   for ( var i = 0; i < 1000; i ++ ) {
+        //       let particle = new this.THREE.Sprite( material );
+        //       particle.position.x = Math.random() * 2 - 1;
+        //       particle.position.y = Math.random() * 2 - 1;
+        //       particle.position.z = Math.random() * 2 - 1;
+        //       particle.position.normalize();
+        //       particle.position.multiplyScalar( Math.random() * 10 + 450 );
+        //       particle.scale.multiplyScalar( 2 );
+        //       particle.geometry = new this.THREE.CircleGeometry();
+        //       this.scene.add( particle );
+        //   }
+        //   // lines
+        //   for (var i = 0; i < 300; i++) {
+        //       var geometry = new this.THREE.Geometry();
+        //       var vertex = new this.THREE.Vector3( Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1 );
+        //       vertex.normalize();
+        //       vertex.multiplyScalar( 450 );
+        //       geometry.vertices.push( vertex );
+        //       var vertex2 = vertex.clone();
+        //       vertex2.multiplyScalar( Math.random() * 0.3 + 1 );
+        //       geometry.vertices.push( vertex2 );
+        //       var line = new this.THREE.Line( geometry, new this.THREE.LineBasicMaterial( { color: 0x000000, opacity: Math.random() } ) );
+        //       this.scene.add( line );
+        //   }
+        //
 
-      let timer = RX.Observable.timer(20,20);
-      timer.subscribe(()=>{
-          this.render();
-      });
-  }
-  render() {
-      this.camera.position.x += ( this.mouseX - this.camera.position.x ) * .05;
-      this.camera.position.y += ( - this.mouseY + 200 - this.camera.position.y ) * .05;
-      this.camera.lookAt( this.scene.position );
-      this.renderer.render( this.scene, this.camera );
-  }
+        let timer = RX.Observable.timer(20,20);
+        timer.subscribe(()=>{
+            this.render();
+        });
+    }
+    render() {
+        let mx = this.mouseX;
+        let my = this.mouseY;
+        console.log("x:" + mx + " y:" + my)
+        this.camera.position.x += ( mx*3 -500 - this.camera.position.x ) * .05;
+        this.camera.position.y += ( - my + 200 - this.camera.position.y ) * .05;
+        this.camera.lookAt( this.scene.position );
+        this.renderer.render( this.scene, this.camera );
+    }
 
 }
