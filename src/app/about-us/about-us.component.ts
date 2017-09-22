c040d3b21b851f4b048ad416a8329db2aecf0cc9
peveneven <eveha94@gmail.com>
2017-09-22 13:32:02 +0200

    Updated about-us

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -3,31 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
-  styleUrls: ['./about-us.component.css']
+  styleUrls: ['./about-us.component.scss']
 })
 export class AboutUsComponent implements OnInit {
 
-  last_known_scroll_position = 0;
-  ticking = false;
-  compArray: number[] = [20];
-  currentIndex = 0;
+  members: any[] = [
+    {name: 'Odin Van der Hultgren Horst', dateOfBirth: '03.07.1992'},
+    {name: 'Odin Van der Hultgren Horst', dateOfBirth: '03.07.1992'},
+    {name: 'Even Flem Hagen', dateOfBirth: '03.07.1992'},
+    {name: 'Even Flem Hagen', dateOfBirth: '03.07.1992'},
+    {name: 'Lars Ådne Heimdal', dateOfBirth: '03.07.1992'},
+    {name: 'Lars Ådne Heimdal', dateOfBirth: '03.07.1992'},
+    {name: 'Peter Gustad', dateOfBirth: '03.07.1992'},
+    {name: 'Peter Gustad', dateOfBirth: '03.07.1992'},
+  ];
 
-  constructor() {
-    window.addEventListener('scroll', (e) => {
-      this.last_known_scroll_position = window.scrollY;
-      if (!this.ticking) {
-        window.requestAnimationFrame(() => {
-          this.doSomething(this.last_known_scroll_position);
-          this.ticking = false;
-        });
-      }
-      this.ticking = true;
-    });
-  }
+  constructor() { }
 
-  doSomething(position: any) {
-    console.log('hello');
-  }
 
   ngOnInit() {
   }
