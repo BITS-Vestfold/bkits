0a66b3885ec11fe0bbe725e3cafa10048c3e3d51
SpacyCoder <sral_h@hotmail.com>
2017-09-25 12:45:14 +0200

    removed duplicated lines on "about us" comp and removed unnecerecy dependencies

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -9,17 +9,12 @@ export class AboutUsComponent implements OnInit {
 
   members: any[] = [
     {name: 'Odin Van der Hultgren Horst', dateOfBirth: '03.07.1992'},
-    {name: 'Odin Van der Hultgren Horst', dateOfBirth: '03.07.1992'},
-    {name: 'Even Flem Hagen', dateOfBirth: '03.07.1992'},
     {name: 'Even Flem Hagen', dateOfBirth: '03.07.1992'},
     {name: 'Lars Ådne Heimdal', dateOfBirth: '03.07.1992'},
-    {name: 'Lars Ådne Heimdal', dateOfBirth: '03.07.1992'},
-    {name: 'Peter Gustad', dateOfBirth: '03.07.1992'},
     {name: 'Peter Gustad', dateOfBirth: '03.07.1992'},
   ];
 
   constructor() { }
-
 
   ngOnInit() {
   }
